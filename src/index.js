ece9438c21a9a44e2145013102d2a2b0d09cd388
Your Name <you@example.com>
2022-10-05 22:16:42 +0300

    added js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,67 @@
 import './index.html';
 import './index.scss';
-import code from './img/code.png'
 
-const imgWrap = document.querySelector('.img');
-const img = new Image();
-img.src = code;
-img.width = 700;
-imgWrap.append(img);
+import {modalController} from "./modules/modalController";
+import {selectController} from "./modules/selectController";
+import {showPassword} from "./modules/showPassword";
+import {choicesController} from "./modules/choices";
 
-console.log(mult(3, 4));
-console.log(sum(3, 4));
+const init = () => {
+  modalController( {
+    modal: '.modal_sign-in',
+    btnOpen: '.header__auth-btn_sign-in',
+    btnClose: '.modal__close'
+  });
+
+  modalController( {
+    modal: '.modal_sign-up',
+    btnOpen: '.header__auth-btn_sign-up',
+    btnClose: '.modal__close'
+  });
+
+  const modalPerson = modalController( {
+    modal: '.modal_person',
+    btnOpen: '.service',
+    parrentBtns: '.services__list',
+    btnClose: '.modal__close',
+    handlerOpenModal: async () => {
+      const data = await fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => response.json());
+
+      const comments = document.querySelectorAll('.review__text');
+
+      comments.forEach((comment) => {
+        if (comment.scrollHeight > 38) {
+          const button = document.createElement('button');
+          button.classList.add('review__open');
+          button.textContent = 'Развернуть';
+          comment.after(button);
+
+          button.addEventListener( 'click', () => {
+            comment.classList.toggle('review__text_open');
+            button.textContent = comment.classList.contains('review__text_open')
+              ? 'Свернуть'
+              : 'Развернуть';
+          })
+        }
+      })
+    }
+  });
+
+  selectController({
+    openBtn: '.category__title',
+    openBlock: '.category__list',
+    closeBtn: '.category__btn',
+    handlerChange: (value) => {
+      console.log(value);
+    }
+
+  });
+
+  showPassword();
+
+  choicesController();
+
+};
+
+init();
